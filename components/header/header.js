c32388b73d4ee52f0aaad989dbc307c854d5cd42
Grisha Pushkov <grisha@push.org.ru>
2016-09-12 22:20:01 +0300

    contact popup

--- components/header/header.js
+++ components/header/header.js
@@ -2,7 +2,7 @@ window.app.registerComponent('header', function($) {
   return {
     init: function() {
       $('#opinions-toggle').click(function() {
-        window.app.popup.show('feedback')
+        window.app.popup.show('opinions')
       })
     }
   }
