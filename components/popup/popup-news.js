0a060df34323bb8c14c03fd9654d760c414b1970
Novikov Bogdan <hcbogdan@gmail.com>
2017-08-15 17:56:31 +0300

    update

--- components/popup/popup-news.js
+++ components/popup/popup-news.js
@@ -1,21 +1,21 @@
-function isShowNewsModal()
-{
-  var a = new Date();
-  var b = new Date(2017, 6, 1);
-  var _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-  // Discard the time and time-zone information.
-  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY) >= 0;
-};
+// function isShowNewsModal()
+// {
+//   var a = new Date();
+//   var b = new Date(2017, 6, 1);
+//   var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+//
+//   // Discard the time and time-zone information.
+//   var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+//   var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+//   return Math.floor((utc2 - utc1) / _MS_PER_DAY) >= 0;
+// };
 
 window.app.registerComponent('popup-news', function($) {
   return {
     init: function() {
-      if (!Cookies.get('msk') && isShowNewsModal()) {
+      if (!Cookies.get('q')) {
         jQuery('#news-modal').modal('show');
-        Cookies.set('msk', 1);
+        Cookies.set('q', 1);
       }
     },
   }
