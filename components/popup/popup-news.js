377d269c59d5e320f550111acb83cb7bc39c49dc
Novikov Bogdan <hcbogdan@gmail.com>
2018-01-24 16:27:53 +0200

    update

--- components/popup/popup-news.js
+++ components/popup/popup-news.js
@@ -1,13 +1,13 @@
 
-window.app.registerComponent('popup-news', function($) {
-  return {
-    init: function() {
-      var cookieName = 'q7';
-      if (Cookies.get(cookieName)) {
-        return;
-      }
-      Cookies.set(cookieName, 1, { expires: 7 });
-      jQuery('#news-modal').modal('show');
-    },
-  }
-});
+// window.app.registerComponent('popup-news', function($) {
+//   return {
+//     init: function() {
+//       var cookieName = 'q7';
+//       if (Cookies.get(cookieName)) {
+//         return;
+//       }
+//       Cookies.set(cookieName, 1, { expires: 7 });
+//       jQuery('#news-modal').modal('show');
+//     },
+//   }
+// });
