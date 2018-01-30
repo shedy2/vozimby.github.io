520ac620edb9be285a3589e3e4f8dcdbb3912762
Novikov Bogdan <hcbogdan@gmail.com>
2018-01-30 17:20:21 +0200

    update

--- components/popup/popup-spb.js
+++ components/popup/popup-spb.js
@@ -1,13 +1,12 @@
-
-window.app.registerComponent('popup-spb', function($) {
-  return {
-    init: function() {
-      var cookieName = 'qspb';
-      if (Cookies.get(cookieName)) {
-        return;
-      }
-      Cookies.set(cookieName, 1, { expires: 7 });
-      jQuery('#popup-spb').modal('show');
-    },
-  }
-});
+// window.app.registerComponent('popup-spb', function($) {
+//   return {
+//     init: function() {
+//       var cookieName = 'qspb';
+//       if (Cookies.get(cookieName)) {
+//         return;
+//       }
+//       Cookies.set(cookieName, 1, { expires: 7 });
+//       jQuery('#popup-spb').modal('show');
+//     },
+//   }
+// });
