1be9d86d9769e0645667734cd3f8cf9adbd5c64d
justique <daewooby@gmail.com>
2018-11-29 11:35:28 +0300

    Add china page, change navigation block

--- assets/app.js
+++ assets/app.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $(".phone_mask").inputmask('+375 (99) 999-99-99', {
        
     });
+
+    $('.to_form').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $(".vz-china__form").offset().top
+        }, 500);
+
+        $('.vz-china__form').find('[name="phone"]').focus();
+        return false;
+    })
 })
